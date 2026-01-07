c0fec3c80ea6e8bc8fce118593886ffe10ae418e
lululthfh <punyalulu05@gmail.com>
2026-01-08 05:01:07 +0700

    feat: add routes for product, order, and item order

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+const productRoutes = require('./routes/product');
+const orderRoutes = require('./routes/order');
+const itemOrderRoutes = require('./routes/order-item');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/user', userRoutes);
+app.use('/product', productRoutes);
+app.use('/order', orderRoutes);
+app.use('/order-item', itemOrderRoutes);
+app.listen(3000,'0.0.0.0', () => {
+    console.log("Server berjalan di port 3000");
+});
